84083c0aeb8775ca84184f19d9e96b940962eda9
Miu <kokocrunchsh@gmail.com>
2022-11-26 21:59:31 +0800

    chore: add exponentia and remove old projects

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,6 +25,7 @@ import Work from "$lib/components/work.svelte";
 	<link rel="preload" as="image" href="https://cdn.mihou.pw/Celestia.png"/>
 	<link rel="preload" as="image" href="https://cdn.mihou.pw/Flora.png"/>
 	<link rel="preload" as="image" href="https://media.discordapp.net/avatars/515067662028636170/47e5abaedee90b70a37826ee3d7435e6.png?size=512"/>
+	<link rel="preload" as="image" href="https://exponie.me/favicon.png"/>
 </svelte:head>
 
 <div class="py-6 limit-width w-full limit-x animate-in fade-in slide-in-from-top-3 ease-in-out duration-1000">
@@ -54,34 +55,22 @@ import Work from "$lib/components/work.svelte";
 </div>
 <div class="pt-2 limit-width w-full flex flex-col md:grid md:grid-cols-4 gap-2 md:gap-4 limit-x animate-in fade-in slide-in-from-top-1 ease-in-out duration-1000">
 	<Work 
-		icon="https://cdn.discordapp.com/avatars/983449886567583744/ddaaa9e3ef8cfc724ba42c44cc54b265.png?size=512"
-		href="https://bellus.quest" 
-		name="Bellus" 
-		desc="Discord Bot, Creator"
-	></Work>
-	<Work 
-		icon="https://cdn.discordapp.com/avatars/741288788164345856/a2f5f56f15b4f302426b63fe4f9f4c34.png?size=512" 
-		href="https://manabot.fun" 
-		name="Mana" 
-		desc="Discord Bot, Creator"
-	></Work>
-	<Work 
 		icon="https://media.discordapp.net/avatars/786464598835986483/8175d0e1793e99b786032be669537a4c.png?size=512"
 		href="https://github.com/ManaNet/Amelia" 
 		name="Amelia" 
 		desc="Discord Bot, Creator"
 	></Work>
 	<Work 
-		icon="https://cdn.mihou.pw/Celestia.png"
-		href="https://github.com/ShindouMihou/Celestia" 
-		name="Celestia" 
-		desc="Logging, Creator"
-	></Work>
-	<Work 
 		icon="https://cdn.mihou.pw/Flora.png"
 		href="https://github.com/ShindouMihou/Flora" 
 		name="Flora" 
 		desc="Blogging, Creator"
+	></Work>
+	<Work 
+		icon="https://exponie.me/favicon.png"
+		href="https://exponie.me/" 
+		name="Exponentia" 
+		desc="Spelling Game, Creator"
 	></Work>
 	<Work 
 		icon="https://media.discordapp.net/attachments/802880444877766697/1009585020727480371/Rubber_Duck_in_White.png"
