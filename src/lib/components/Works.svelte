7f8fd56c55cb225bb8a343e5af95df29a6dced66
Miu <kokocrunchsh@gmail.com>
2023-08-26 15:53:05 +0800

    feat: add more works and discontinued

--- src/lib/components/Works.svelte
+++ src/lib/components/Works.svelte
@@ -3,28 +3,28 @@
 </script>
 
 <Work
-        icon="https://cdn.discordapp.com/avatars/983449886567583744/ddaaa9e3ef8cfc724ba42c44cc54b265.png?size=512"
-        href="https://bellus.quest"
-        name="Bellus"
+        icon="https://cdn.discordapp.com/attachments/802880444877766697/1144847567935524905/threadscore.png?size=512"
+        href="https://top.gg/bot/1144365871356194866"
+        name="Threadscore"
         desc="Discord Bot, Creator"
 ></Work>
 <Work
-        icon="https://cdn.discordapp.com/avatars/741288788164345856/a2f5f56f15b4f302426b63fe4f9f4c34.png?size=512"
-        href="https://github.com/ManaNet"
-        name="Mana"
-        desc="Discord Bot, Creator"
+        icon="https://cdn.discordapp.com/attachments/1144617800204300288/1144901046217494538/primrose.png?size=512"
+        href="https://github.com/ShindouMihou/primrose"
+        name="Primrose"
+        desc="Blogging Platform, Creator"
 ></Work>
 <Work
-        icon="https://media.discordapp.net/avatars/786464598835986483/8175d0e1793e99b786032be669537a4c.png?size=512"
-        href="https://github.com/ManaNet/Amelia"
+        icon="https://cdn.discordapp.com/attachments/1144617800204300288/1144902097867911218/burp.png"
+        href="https://github.com/ShindouMihou/burp"
         name="Amelia"
         desc="Discord Bot, Creator"
 ></Work>
 <Work
-        icon="https://cdn.mihou.pw/Celestia.png"
-        href="https://github.com/ShindouMihou/Celestia"
-        name="Celestia"
-        desc="Logging, Creator"
+        icon="https://cdn.discordapp.com/attachments/802880444877766697/1144847567935524905/threadscore.png?size=512"
+        href="https://top.gg/bot/1144365871356194866"
+        name="Burp"
+        desc="Deployment Tool, Creator"
 ></Work>
 <Work
         icon="https://avatars.githubusercontent.com/u/122293977?s=1024&v=4"
@@ -33,16 +33,22 @@
         desc="Discord Bot, Creator"
 ></Work>
 <Work
-        icon="https://cdn.mihou.pw/Flora.png"
-        href="https://github.com/ShindouMihou/Flora"
-        name="Flora"
-        desc="Blogging, Creator"
-></Work>
-<Work
         icon="https://exponie.me/favicon.png"
         href="https://exponie.me/"
         name="Exponentia"
         desc="Spelling Game, Creator"
+></Work>
+<Work
+        icon="https://cdn.discordapp.com/attachments/1144617800204300288/1144900191820980265/Screenshot_2023-08-26_073658_upscayl_4x_realesrgan-x4plus-anime1.png?size=512"
+        href="https://github.com/ShindouMihou/guarde"
+        name="Exponentia (Desktop)"
+        desc="Spelling Game, Creator"
+></Work>
+<Work
+        icon="https://exponie.me/favicon.png"
+        href="https://exponie.me/"
+        name="Guarde"
+        desc="Reverse Proxy, Creator"
 ></Work>
 <Work
         icon="https://media.discordapp.net/attachments/802880444877766697/1009585020727480371/Rubber_Duck_in_White.png"
@@ -55,4 +61,28 @@
         href="https://beemo.gg"
         name="Beemo"
         desc="Discord Bot, Co-Developer"
+></Work>
+<Work
+        icon="https://cdn.discordapp.com/avatars/983449886567583744/ddaaa9e3ef8cfc724ba42c44cc54b265.png?size=512"
+        href="https://irina.pages.dev"
+        name="Bellus (discontinued)"
+        desc="Discord Bot, Creator"
+></Work>
+<Work
+        icon="https://cdn.discordapp.com/avatars/741288788164345856/a2f5f56f15b4f302426b63fe4f9f4c34.png?size=512"
+        href="https://github.com/ManaNet"
+        name="Mana (discontinued)"
+        desc="Discord Bot, Creator"
+></Work>
+<Work
+        icon="https://cdn.mihou.pw/Flora.png"
+        href="https://github.com/ShindouMihou/Flora"
+        name="Flora (discontinued)"
+        desc="Blogging, Creator"
+></Work>
+<Work
+        icon="https://cdn.mihou.pw/Celestia.png"
+        href="https://github.com/ShindouMihou/Celestia"
+        name="Celestia (discontinued)"
+        desc="Logging, Creator"
 ></Work>
