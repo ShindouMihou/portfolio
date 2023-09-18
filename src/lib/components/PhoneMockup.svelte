40d2b18063a74fffc5d706a3181424f69c5dbd09
Shindou Mihou <git@mihou.pw>
2023-09-18 21:04:37 +0800

    feat: add on-and-off state

--- src/lib/components/PhoneMockup.svelte
+++ src/lib/components/PhoneMockup.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     export let background = "bg-white"
     export let padding = "p-4"
+
+    let status = true
+
+    function switchOnOff() {
+        status = !status
+    }
 </script>
 <style>
     .mockup-contents::-webkit-scrollbar {
@@ -15,8 +21,12 @@
     <div class="h-[32px] w-[3px] bg-black absolute -left-[17px] top-[72px] rounded-l-lg"></div>
     <div class="h-[46px] w-[3px] bg-black absolute -left-[17px] top-[124px] rounded-l-lg"></div>
     <div class="h-[46px] w-[3px] bg-black absolute -left-[17px] top-[178px] rounded-l-lg"></div>
-    <div class="h-[64px] w-[3px] bg-black absolute -right-[17px] top-[142px] rounded-r-lg"></div>
+    <button on:click={switchOnOff} class="h-[64px] w-[3px] bg-black absolute -right-[17px] top-[142px] rounded-r-lg"></button>
     <div class="mockup-contents rounded-[2rem] rounded-b-none 2xl:rounded-b-[2rem] overflow-x-hidden overflow-y-scroll w-[272px] h-[572px] max-h-[567px] 2xl:max-h-full {background} {padding}">
-        <slot/>
+        {#if status}
+            <slot/>
+        {:else}
+            <div class="h-full w-full bg-black"/>
+        {/if}
     </div>
 </div>
