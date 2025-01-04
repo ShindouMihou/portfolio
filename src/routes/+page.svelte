d526d1e86bb2e38e05218efc913c5baef5d8e782
Miu <kokocrunchsh@gmail.com>
2025-01-04 19:18:14 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,16 +16,16 @@
 
     onMount(() => {
         setTimeout(() => { show = true}, 500)
-        setTimeout(() => { $background = 'bg-white'; $text = 'text-black'; $splashscreen = false; turnOnPhone = true; }, 6_000)
+        setTimeout(() => { $background = 'bg-white'; $text = 'text-black'; $splashscreen = false; turnOnPhone = true; }, 1_500)
         setTimeout(() => incrementFrame(), 800)
     })
 
     function incrementFrame() {
-        if (frame > 4) return
+        if (frame > 1) return
         setTimeout(() => {
             frame++
             incrementFrame()
-        }, frame === 2 ? 1_000 : frame === 3 ? 1_500 : frame === 4 ? 600 : 800)
+        }, 1_500)
     }
 </script>
 
@@ -40,35 +40,17 @@
 </style>
 {#if show}
     <div class="w-screen">
-        <div class="flex flex-col items-center pt-6 md:pt-24 pb-12 {frame < 5 ? 'align-middle min-h-screen justify-center' : ''}">
+        <div class="flex flex-col items-center pt-6 md:pt-24 pb-12 {frame < 2 ? 'align-middle min-h-screen justify-center' : ''}">
             {#if frame === 0}
-                <div in:slide={{ duration: 700 }} out:slide={{ duration: 300}}>
-                    <h1 class="text-2xl playfair lowercase font-bold">i am me.</h1>
-                </div>
-            {:else if frame === 1}
-                <div in:slide={{ duration: 700 }} out:blur={{ duration: 300}}>
-                    <h1 class="text-2xl playfair lowercase font-bold">i am myself.</h1>
-                </div>
-            {:else if frame === 2}
-                <div in:blur={{ delay: 200, duration: 700 }} out:blur={{ duration: 300 }}>
-                    <h1 class="text-2xl playfair lowercase glow font-bold">i am.</h1>
-                </div>
-            {:else if frame === 3}
                 <div class="flex flex-col items-center" in:blur={{ delay: 300, duration: 700 }} out:blur={{ duration: 300}}>
-                    <h1 class="text-2xl playfair lowercase glow font-bold">shindou mihou</h1>
-                    <h2 class="text-sm leading-none playfair lowercase">a curious and adventurous student.</h2>
-                </div>
-            {:else if frame === 4}
-            {:else if frame === 5}
-                <div class="flex flex-col items-center" in:blur={{ duration: 700 }}>
                     <h1 class="text-2xl playfair lowercase glow font-bold">shindou mihou</h1>
                     <h2 class="text-sm leading-none playfair lowercase">a curious and adventurous student.</h2>
                 </div>
             {/if}
         </div>
-        <div in:blur={{ duration: 700, delay: 6_000 }} out:slide>
+        <div in:blur={{ duration: 700, delay: 2_000 }} out:slide>
             <PhoneMockup padding="p-0" bind:turnOn={turnOnPhone}>
-                <div in:fade={{delay: 2_400}}>
+                <div in:fade={{delay: 400}}>
                     <StatusBar/>
                     {#if $currentPage === "introduction"}
                         <Introduction/>
