e410e66a139871f92f293d60649d385484664571
Shindou Mihou <git@mihou.pw>
2025-04-13 20:19:22 +0800

    fix: web images not loading due to invalid cloudflare r2 url

--- src/components/ViewableApplications.tsx
+++ src/components/ViewableApplications.tsx
@@ -7,17 +7,17 @@ export default function ViewableApplications() {
         <div className={"flex flex-row flex-wrap gap-4 py-4 mx-auto items-center justify-center"}>
             <SelectableApplication
                 domain={"https://flyght.mihou.dev"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/flyght-profile.png"}
+                image={"https://portfolio-image-assets.mihou.dev/flyght-profile.png"}
                 alt={"Flyght"}
             />
             <SelectableApplication
                 domain={"https://threadscore.mihou.dev"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/threadscore.png"}
+                image={"https://portfolio-image-assets.mihou.dev/threadscore.png"}
                 alt={"Threadscore"}
             />
             <SelectableApplication
                 domain={"https://blog.mihou.dev"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/primrose.png"}
+                image={"https://portfolio-image-assets.mihou.dev/primrose.png"}
                 alt={"Primrose"}
             />
             <SelectableApplication
@@ -27,17 +27,17 @@ export default function ViewableApplications() {
             />
             <SelectableApplication
                 domain={"https://beemo.gg"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/47e5abaedee90b70a37826ee3d7435e6.png"}
+                image={"https://portfolio-image-assets.mihou.dev/47e5abaedee90b70a37826ee3d7435e6.png"}
                 alt={"Beemo"}
             />
             <SelectableApplication
                 domain={"https://irina.pages.dev"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/ddaaa9e3ef8cfc724ba42c44cc54b265.png"}
+                image={"https://portfolio-image-assets.mihou.dev/ddaaa9e3ef8cfc724ba42c44cc54b265.png"}
                 alt={"Bellus"}
             />
             <SelectableApplication
                 domain={"https://rubberduck.mihou.dev"}
-                image={"https://pub-084dc2a53d6c4eb59db97bcd1add47d3.r2.dev/Rubber_Duck_in_White.png"}
+                image={"https://portfolio-image-assets.mihou.dev/Rubber_Duck_in_White.png"}
                 alt={"My Rubber Duck"}
             />
         </div>
