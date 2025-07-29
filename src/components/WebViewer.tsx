cb7d309420a6b6f2dd0b6c9abf82bbf27990b175
ShindouMihou <git@mihou.dev>
2025-07-29 22:07:17 +0800

    PF-3: Update the webviewer to show the domain link

--- src/components/WebViewer.tsx
+++ src/components/WebViewer.tsx
@@ -2,6 +2,7 @@
 
 import {useSelector} from "react-redux";
 import {RootState} from "@/stores/global";
+import {ArrowLeft, ArrowRight, Ellipsis, RotateCw} from "lucide-react";
 
 export default function WebViewer() {
     const currentDomain = useSelector((state: RootState) => state.domainSlice)
@@ -13,13 +14,31 @@ export default function WebViewer() {
                 <div className={"w-full bg-zinc-900 h-full p-4 text-white font-spaceMono"}>
                 </div>
             ) : (
-                <iframe src={currentDomain.value}
-                        id={"embed"}
-                        className={"overflow-auto bg-white w-full h-full border-none scrollbar-none"}
-                        seamless={true}
-                        scrolling={"auto"}
-                >
-                </iframe>
+                <div className={"w-full h-full"}>
+                    <div className={"flex flex-row items-center justify-between px-6 py-2"}>
+                        <div className={"flex flex-row items-center gap-2"}>
+                            <div className={"w-2 h-2 bg-red-500 rounded-full"}/>
+                            <div className={"w-2 h-2 bg-yellow-500 rounded-full"}/>
+                            <div className={"w-2 h-2 bg-green-500 rounded-full"}/>
+                            <ArrowLeft size={14} className={"text-gray-500 ml-2"}/>
+                            <ArrowRight size={14} className={"text-gray-500"}/>
+                            <RotateCw size={14} className={"text-gray-500"}/>
+                        </div>
+                        <a href={currentDomain.value} target={"_blank"} className={"py-1 px-3 rounded bg-zinc-900 w-[65%] h-fit"}>
+                            <h3 className={"text-xs"}>{currentDomain.value.replaceAll("https://", "")}</h3>
+                        </a>
+                        <div>
+                            <Ellipsis size={14} className={"text-gray-500"}/>
+                        </div>
+                    </div>
+                    <iframe src={currentDomain.value}
+                            id={"embed"}
+                            className={"overflow-auto bg-white w-full h-full border-none scrollbar-none"}
+                            seamless={true}
+                            scrolling={"auto"}
+                    >
+                    </iframe>
+                </div>
             )}
         </div>
     )
